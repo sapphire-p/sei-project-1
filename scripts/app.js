38c5b87894f81ea6681c05bbc13e520fe3c82b08
Sapphire Paston <sapphire.paston@gmail.com>
2021-10-08 14:23:23 +0100

    Added some image assets and declared fundamental variables

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,42 @@
 
-const grid = document.querySelector(".grid")
+// Grid variables
 
-const width = 10
+const grid = document.querySelector(".grid")
+const width = 11
 const cellCount = width * width
 const cells = []
+
+// Scoring variables
+
+let score = 0
+let livesRemaining = 3
+
+// Gun, Dinos, Missile, Rock position variables
+
+const gunStartPosition = 104 // index number
+let gunCurrentPosition // index number
+
+// const dinosStartPosition // array of index numbers
+let dinosCurrentPosition //array of index numbers
+
+let missileCurrentPosition // index number
+let rockCurrentPosition // index number
+
+// Timer variables
+
+let dinosTimer
+let missileTimer
+let rockLaunchTimer
+let rockTimer
+
+// Speed variables
+
+let dinosSpeed = 1000
+let rockSpeed = 1000
+let missileSpeed = 1000
+
+
+
 
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
@@ -16,3 +49,7 @@ function createGrid() {
 }
 
 createGrid()
+
+cells[gunStartPosition].classList.add("gun")
+cells[82].classList.add("dino")
+cells[93].classList.add("missile")
