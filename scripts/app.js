5b089c6af553ae641886b49942ec334d5b34c3b6
Sapphire Paston <sapphire.paston@gmail.com>
2021-10-08 15:23:24 +0100

    Added addItem(itemClass, position) and removeItem(itemClass, position) functions

--- scripts/app.js
+++ scripts/app.js
@@ -11,16 +11,21 @@ const cells = []
 let score = 0
 let livesRemaining = 3
 
-// Gun, Dinos, Missile, Rock position variables
+// Gun, Dinos, Missile, Rock position and class variables
 
 const gunStartPosition = 104 // index number
 let gunCurrentPosition // index number
+const gunClass = "gun"
 
-// const dinosStartPosition // array of index numbers
-let dinosCurrentPosition //array of index numbers
+const dinosStartPosition = [26, 27, 28] // array of index numbers
+let dinosCurrentPosition // array of index numbers
+const dinoClass = "dino"
 
 let missileCurrentPosition // index number
+const missileClass = "missile"
+
 let rockCurrentPosition // index number
+const rockClass = "rock"
 
 // Timer variables
 
@@ -40,8 +45,12 @@ let missileSpeed = 1000
 const startButton = document.querySelector(".start-button")
 startButton.addEventListener("click", startGame)
 
+// document.addEventListener('keyup', keysGunAction)
+
+
+
 function startGame() {
-  console.log("Game started!")
+  console.log("startGame() function called")
 }
 
 function createGrid() {
@@ -56,6 +65,28 @@ function createGrid() {
 
 createGrid()
 
-cells[gunStartPosition].classList.add("gun")
-cells[82].classList.add("dino")
-cells[93].classList.add("missile")
+
+function addItem(itemClass, position) {
+  // add some control flow with conditional logic to say "If itemClass is gunClass, do this, else if itemClass is dinoClass, do this"
+  cells[position].classList.add(itemClass)
+}
+
+function removeItem(itemClass, position) {
+  // add some control flow with conditional logic to say "If itemClass is gunClass, do this, else if itemClass is dinoClass, do this"
+  cells[position].classList.remove(itemClass)
+}
+
+
+
+// function keysGunAction(event) {
+//   console.log("keysGunAction() function called")
+//   const key = event.keyCode
+
+//   if ()
+// }
+
+
+
+// cells[gunStartPosition].classList.add("gun")
+// cells[82].classList.add("dino")
+// cells[93].classList.add("missile")
